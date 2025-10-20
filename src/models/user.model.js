befe087322e2e946190a491d70d31594f225035f
Kumail Abbas <kkumailabbas142@gmail.com>
2025-10-20 21:09:21 +0500

    add register user and fix bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new Schema(
             unique: true,
             lowercase: true,
             trim: true,
-            index: true //searching feild optimize
+            index: true //searching field optimize
         },
         email: {
             type: String,
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function(next) {
     if(this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
