cd30a1fa24cd7c95f75e06c70c1b9d6049f276c8
Kumail Abbas <kkumailabbas142@gmail.com>
2025-10-22 18:28:20 +0500

    user controller and postman configure and debug error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,10 +21,10 @@ const registerUser = asyncHandler(async (req, res) =>{
    if (
     [fullname,email,username,password].some((field) => field?.trim() === "")
    ) {
-      throw ApiError(400, "All field are required")
+      throw new ApiError(400, "All field are required")
    }
 
-   const existedUser = User.findOne({
+   const existedUser = await User.findOne({
     $or: [{ username } , { email }]
    })
 
@@ -33,7 +33,11 @@ const registerUser = asyncHandler(async (req, res) =>{
    }
    
    const avatarLocalPath = req.files?.avatar[0]?.path;
-   const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+   // const coverImageLocalPath =  req.files?.coverImage[0]?.path;
+   let coverImageLocalPath;
+   if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+      coverImageLocalPath = req.files.coverImage[0].path
+   }
 
    if (!avatarLocalPath) {
       throw new ApiError(400, "avatar file is required")
@@ -55,7 +59,7 @@ const registerUser = asyncHandler(async (req, res) =>{
     username: username.toLowerCase()
    })
 
-   const createdUser = await user.findById(user._id).select(
+   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
    )
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,8 +14,10 @@ const uploadOnCloudinary = async (localFilePath) =>{
       resource_type:'auto'
     })
     //file has uploaded successfully
-    console.log("file is uploaded on cloudinary", response.url);
+    // console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath)
     return response;
+    
   } catch (error) {
       fs.unlinkSync(localFilePath) // remove the locally saves temporary file as the upload operation got failed
       return null;
